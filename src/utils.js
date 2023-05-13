c5ddfe0de0d5135bbdf1bf5178d82d85d5893941
shortercode <iainshorter@gmail.com>
2023-05-13 16:15:41 -0600

    add util for checking compilation errors

--- src/utils.js
+++ src/utils.js
@@ -4,24 +4,39 @@
 * @param {T | undefined | null} value
 * @returns {value is T}
 */
-function isDefined (value) {
+function isDefined(value) {
   return value !== undefined && value !== null;
- }
- 
- class Panic extends Error {
+}
+
+/**
+ * 
+ * @param {GPUShaderModule} module 
+ * @returns {Promise<void>}
+ */
+async function check_compilation_errors (module) {
+  const info = await module.getCompilationInfo();
+  if (info.messages.length > 0) {
+    for (const msg of info.messages) {
+      console.error(`${module.label} ${msg.message}@${msg.lineNum}`)
+    }
+  }
+  Panic.About(`Shader compilation failed`);
+}
+
+class Panic extends Error {
   /**
    * @param {string} msg
    */
   constructor(msg) {
     super(msg);
   }
- 
+
   /**
    * @param {boolean} condition
    * @param {string} msg
    * @return {asserts condition}
    */
-  static Assert (condition, msg) {
+  static Assert(condition, msg) {
     if (!condition) {
       Panic.About(msg);
     }
@@ -31,7 +46,7 @@ function isDefined (value) {
    * @param {string} msg
    * @return {never}
    */
-  static About (msg) {
+  static About(msg) {
     throw new Panic(msg);
   }
- }
+}
